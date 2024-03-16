97814a0596d10a9544caaa5567a32a8739bc8c30
TvoiiSon <andreylastovetskiy@gmail.com>
2024-03-16 19:50:03 +0300

    create form to create request for workers

--- index.php
+++ index.php
@@ -23,25 +23,6 @@ require_once("./db/db.php");
         if($_COOKIE['id_group'] == 1){ ?>
             | <a href="./statistic.php">Статистика</a>
             <div class="container">
-                <div class="container-left">
-                    <h2>Зарегистрировать Сотрудника</h2>
-                    <form action="./vendor/create-worker.php" method="post" style="display: flex; flex-direction: column; width: 350px; gap: 10px;">
-                        <input type="text" name="login" placeholder="Логин" required>
-                        <input type="email" name="email" placeholder="Email" required>
-                        <input type="text" name="phone" placeholder="Телефон" required>
-                        <input type="password" name="password" placeholder="Пароль" required>
-                        <input type="password" name="cpassword" placeholder="Подтверждение Пароля" required>
-                        <input type="submit" value="Зарегистрировать">
-                    </form>
-                    <?php
-                        if(empty($_SESSION['errCreateWorker'])) {
-                            echo "";
-                        } else {
-                            echo $_SESSION['errCreateWorker'];
-                        }
-                        session_destroy();
-                    ?>
-                </div>
                 <div class="container-right">
                     <h2>Сотрудники</h2>
                     <?php 
@@ -55,8 +36,7 @@ require_once("./db/db.php");
                                     <p>Логин сотрудника - <?= $worker[2] ?></p>
                                     <p>Email сотрудника - <?= $worker[4] ?></p>
                                     <p>Телефон сотрудника - <?= $worker[5] ?></p>
-                                    <a href="./appoint-worker.php?id_worker=<?= $worker[0] ?>">Назначить работника</a> | 
-                                    <a href="./vendor/delete-worker.php?id_worker=<?= $worker[0] ?>">Уволить</a>
+                                    <a href="./appoint-worker.php?id_worker=<?= $worker[0] ?>">Назначить работника</a>
                                 </div>
                             </li>
                         <?php } ?>
@@ -64,7 +44,7 @@ require_once("./db/db.php");
                 </div>
             </div>
         <?php } elseif($_COOKIE['id_group'] == 2) { ?>
-            <div class="container">
+            <div class="container" style="display: flex; justify-content: space-between;">
                 <div class="container-left">
                     <h2>Обородования для ремонта</h2>
                     <?php 
@@ -126,6 +106,72 @@ require_once("./db/db.php");
                         <?php } ?>
                     </ul>
                 </div>
+                <div class="container-right">
+                    <?php
+                        $select_type_of_faults = mysqli_query($connect, "SELECT * FROM `type_of_fault`");
+                        $select_type_of_faults = mysqli_fetch_all($select_type_of_faults);
+                    ?>
+                    <div>
+                        <h2>Отправить заявку на ремонт</h2>
+                        <form action="./vendor/create-request.php" method="post" style="display: flex; flex-direction: column; width: 350px; gap: 10px;">
+                            <input type="hidden" name="id_user" value="<?= $_COOKIE['id_user'] ?>">
+                            <input type="hidden" name="date_create" value="<?= date("Y-m-d")  ?>">
+                            <input type="text" name="equipment" placeholder="Название оборудования" required>
+                            <select name="type_of_fault" required>
+                                <?php foreach($select_type_of_faults as $type_of_fault) { ?>
+                                    <option value="<?= $type_of_fault[0] ?>"><?= $type_of_fault[1] ?></option>
+                                <?php } ?>
+                            </select>
+                            <textarea name="description" cols="30" rows="10" placeholder="Описание проблемы" required></textarea>
+                            <input type="submit" value="Подать заявку">
+                        </form>
+                        <?php
+                            if(empty($_SESSION['errCreateRequest'])) {
+                                echo "";
+                            } else {
+                                echo $_SESSION['errCreateRequest'];
+                            }
+                            session_destroy();
+                        ?>
+                    </div>
+                    <div>
+                        <h2>Мои заявки на ремонт</h2>
+                        <?php 
+                            $id_user = $_COOKIE['id_user'];
+                            $select_requests = mysqli_query($connect, "SELECT * FROM `requests` WHERE `id_client`='$id_user'");
+                            $select_requests = mysqli_fetch_all($select_requests);
+                        ?>
+                        <ul>
+                            <?php foreach ($select_requests as $request) { 
+                                if($request[7] == 0) {
+                                    $ready = "Ожидает ремонта";
+                                } elseif($request[7] == 1) {
+                                    $ready = "Находится в ремонте";
+                                } elseif($request[7] == 2) {
+                                    $ready = "Ремонт окончен";
+                                }
+                                $id_type_of_fault = $request[4];
+                                $select_type_of_fault = mysqli_query($connect, "SELECT * FROM `type_of_fault` WHERE `id`='$id_type_of_fault'");
+                                $select_type_of_fault = mysqli_fetch_assoc($select_type_of_fault);
+                                ?>
+                                <li>
+                                    <div class="li-content">
+                                        <p>Название оборудования - <span><?= $request[3] ?></span></p>
+                                        <p>Тип неисправности - <span><?= $select_type_of_fault['name_type'] ?></span></p>
+                                        <p>Описание проблемы - <span><?= $request[5] ?></span></p>
+                                        <p>Дата создания заявки - <span><?= $request[6] ?></span></p>
+                                        <p>Статус - <?= $ready ?></p>
+                                        <br>
+                                        <?php if($request[7] == 0) { ?>
+                                            <a href="./edit-request.php?id_request=<?= $request[0] ?>">Редактировать</a> | 
+                                            <a href="./vendor/delete-request.php?id_request=<?= $request[0] ?>">Удалить</a>
+                                        <?php } ?>
+                                    </div>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                </div>
             </div>
         <?php } elseif($_COOKIE['id_group'] == 3) { ?>
             <?php
@@ -139,7 +185,6 @@ require_once("./db/db.php");
                         <input type="hidden" name="id_user" value="<?= $_COOKIE['id_user'] ?>">
                         <input type="hidden" name="date_create" value="<?= date("Y-m-d")  ?>">
                         <input type="text" name="equipment" placeholder="Название оборудования" required>
-                        <!-- <input type="text" name="type_of_fault" placeholder="Тип неисправности" required> -->
                         <select name="type_of_fault" required>
                             <?php foreach($select_type_of_faults as $type_of_fault) { ?>
                                 <option value="<?= $type_of_fault[0] ?>"><?= $type_of_fault[1] ?></option>
